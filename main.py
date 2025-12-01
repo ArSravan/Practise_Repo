344ed4193e1de11f059a669c36e929733109503e
ArSravan <arsravan88@gmail.com>
2025-12-01 22:57:46 +0100

    Created main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+def main() ->list[int]:
+    """_summary_
+
+    :return: _description_
+    :rtype: list[int]
+    """
+    _list = [random.randint(0,20) for i in range(50)]
+
+    return _list
